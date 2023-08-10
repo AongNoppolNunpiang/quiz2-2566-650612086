0fbb80d2889ea5903f13abe2808d32b155c8d0d9
Aong noppol Nunpiang <nopphol1122@gmail.com>
2023-08-10 14:59:46 +0700

    s

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,8 @@
 "use client";
-
+import { Comment } from "@/components/Comment";
+import { comments } from "@/libs/comments";
+import { useState } from "react";
+import { PostOwner } from "@/components/PostOwner";
 export default function HomePage() {
   return (
     <div
@@ -12,26 +15,7 @@ export default function HomePage() {
         className="mx-auto p-3 rounded rounded-3 shadow-sm bg-white"
       >
         {/* Post Owner Example*/}
-        <div className="vstack gap-3">
-          <div className="d-flex align-items-center gap-3">
-            <img
-              src="https://scontent.fbkk29-5.fna.fbcdn.net/v/t39.30808-6/245059915_1054455365327279_6176826216884415606_n.jpg?_nc_cat=107&cb=99be929b-59f725be&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFLZLqc-4MC_cRLwOGtqhYLhCn2jN3q1VmEKfaM3erVWUg4iEiGbURjHt_Cjjt2RIJoydYlTCAJbHb2QOygKlrw&_nc_ohc=I48pQ4XsVewAX9lztPx&_nc_ht=scontent.fbkk29-5.fna&oh=00_AfA9y2Hvg7rmvmwubpr1HdR6TDO-ibT9pR9Dq-UCym5-Fw&oe=64D9DAE0"
-              width="48"
-              height="48"
-              className="rounded-circle"
-              style={{ objectFit: "cover" }}
-            />
-            <span className="fw-semibold fs-5">Noppol Nunpiang 650612086</span>
-          </div>
-
-          <span>Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207</span>
-
-          <div className="d-flex align-items-center gap-1">
-            <img src="/like.svg" width={20}></img>
-            <span className="text-muted">100 คน</span>
-          </div>
-          <hr className="m-0 border" />
-        </div>
+        <PostOwner />
 
         {/* Comment Example */}
         <div className="d-flex gap-2 my-2">
